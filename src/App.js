ff9d49f19b43356ec3dd7afb688d2a6a45362870
Guy Whale <guywhale@gmail.com>
2022-08-10 14:33:11 +0100

    Set up AddMovie component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -73,6 +74,10 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
+
 	let content = <p>No movies found.</p>;
 
 	if (movies.length > 0) {
@@ -89,6 +94,9 @@ function App() {
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
